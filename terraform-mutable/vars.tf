77fe401e7ddb20be168bf420c62ce1cf2ed216d2
mvpc-11232 <mvp2612@hotmail.com>
2022-11-07 06:57:26 +0530

    Auto Push Commit

--- terraform-mutable/vars.tf
+++ terraform-mutable/vars.tf
@@ -1,9 +1,8 @@
 variable "ENV" {}
 variable "COMPONENT" {}
 
-\
+
 variable "INSTANCE_TYPE" {}
 variable "SPOT_INSTANCE_COUNT" {} 
 variable "OD_INSTANCE_COUNT" {}
-variable "COMPONENT" {}
 variable "APP_PORT" {}
