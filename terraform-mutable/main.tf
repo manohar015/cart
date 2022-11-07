6a463a1e9ffd02ee08fca23267b2110c2c50535b
mvpc-11232 <mvp2612@hotmail.com>
2022-11-07 07:00:39 +0530

    Auto Push Commit

--- terraform-mutable/main.tf
+++ terraform-mutable/main.tf
@@ -5,7 +5,6 @@ module "cart" {
   INSTANCE_TYPE        = var.INSTANCE_TYPE
   SPOT_INSTANCE_COUNT  = var.SPOT_INSTANCE_COUNT
   OD_INSTANCE_COUNT    = var.OD_INSTANCE_COUNT 
-  COMPONENT            = var.COMPONENT 
   APP_PORT             = var.APP_PORT
 }
 
